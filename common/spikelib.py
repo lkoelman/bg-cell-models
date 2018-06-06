17fe658d4cb1097e4b82f55dc662c960664ab88d
Lucas Koelman <lucas.koelman@gmail.com>
2018-06-06 23:05:43 +0100

    Add function to generate bursty spikes with variable IBI and duration

--- common/spikelib.py
+++ common/spikelib.py
@@ -12,6 +12,9 @@ def make_oscillatory_bursts(
     Make oscillatory bursting spike times with given burst period, 
     burst duration, and intra- and inter-burst firing rates.
 
+    The inter-burst interval (IBI) and burst duration are fixed so bursts
+    will occur regularly with fixed intervals.
+
     @param      T_burst : float
                 Burst period (ms)
 
@@ -68,3 +71,86 @@ def make_oscillatory_bursts(
             t += inter_ISIs[i_inter]
             i_inter += 1
             yield t
+
+
+def make_variable_bursts(
+    T_burst, dur_burst, f_intra, f_inter, 
+    max_dur=10e3, rng=None):
+    """
+    Make bursting spike times with given burst period, 
+    burst duration, and intra- and inter-burst firing rates.
+
+    The inter-burst interval (IBI) and burs duration are variable and drawn
+    from a negative exponential distribution so the burst rate is 
+    Poisson-distributed.
+
+    @param      T_burst : float
+                Burst period (ms), mean of negexp distribution
+
+    @param      dur_burst : float
+                Burst duration (ms), mean of negexp distribution
+
+    @param      f_intra : float
+                Intra-burst firing rate (Hz)
+
+    @param      f_inter : float
+                Inter-burst firing rate (Hz)
+
+    @param      rng : numpy.Random
+                Random object (optional)
+
+    @return     Generator object that generates spike times
+
+
+    EXAMPLES
+    --------
+
+    >>> import numpy
+    >>> burst_gen = make_variable_bursts(3500.0, 545.0, 50.0, 5.0)
+    >>> bursty_spikes = numpy.fromiter(burst_gen, float)
+
+    """
+    if rng is None:
+        rng = np.random
+
+    # Blend ISIs from two negexp distributions centered at intra- and
+    # inter-burst firing rate, respectively
+    T_intra, T_inter = 1e3/f_intra, 1e3/f_inter
+    
+    intra_ISIs = rng.exponential(T_intra, size=int(2*max_dur/T_intra))
+    inter_ISIs = rng.exponential(T_inter, size=int(2*max_dur/T_inter))
+    burst_IBIs = rng.exponential(T_burst, size=int(2*max_dur/T_burst))
+    burst_durs = rng.exponential(dur_burst, size=int(2*max_dur/dur_burst))
+    
+    t_start_burst = burst_IBIs[0]
+    t_end_burst = t_start_burst + burst_durs[0]
+
+    # Indices into samples of interval distributions
+    i_IBI, i_dur = 1, 1
+    i_inter, i_intra = 0, 0
+
+    in_burst = False
+    t = 0.0    
+    while t < max_dur:
+        if t < t_start_burst:
+            if in_burst and t < t_end_burst:
+                t += intra_ISIs[i_intra]
+                i_intra += 1
+                yield t
+            elif in_burst and t >= t_end_burst:
+                in_burst = False
+                i_dur += 1
+                t_end_burst = t_start_burst + burst_durs[i_IBI]
+                continue
+            else:
+                t += inter_ISIs[i_inter]
+                i_inter += 1
+                yield t
+        elif t >= t_start_burst:
+            # only reached once per burst
+            in_burst = True
+            i_IBI += 1
+            t_start_burst += burst_IBIs[i_IBI]
+            continue
+            
+            
