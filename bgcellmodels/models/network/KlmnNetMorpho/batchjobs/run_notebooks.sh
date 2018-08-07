4352f6cb5dbcd6b42594cf61544bf73441bed737
Lucas Koelman <lucas.koelman@gmail.com>
2018-08-07 19:07:51 +0100

    Update sweep analysis notebook.

--- bgcellmodels/models/network/KlmnNetMorpho/batchjobs/run_notebooks.sh
+++ bgcellmodels/models/network/KlmnNetMorpho/batchjobs/run_notebooks.sh
@@ -6,25 +6,20 @@ nb_dir="${model_dir}/analysis"
 notebook="${nb_dir}/${nb_filename}"
 logfile="${nb_dir}/nb_exec_list.log"
 conffile="${nb_dir}/nb_exec_conf.py"
+export NB_CONF_FILE=${conffile} # for subprocesses
 
 # List simulation output directories to analyze
-output_dirs=( \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780629.sonic-head_DA-depleted-v3_CTX-favg14_fburst5" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780630.sonic-head_DA-depleted-v3_CTX-favg14_fburst7" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780631.sonic-head_DA-depleted-v3_CTX-favg14_fburst9" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780632.sonic-head_DA-depleted-v3_CTX-favg14_fburst11" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780633.sonic-head_DA-depleted-v3_CTX-favg14_fburst13" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780634.sonic-head_DA-depleted-v3_CTX-favg14_fburst15" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780635.sonic-head_DA-depleted-v3_CTX-favg14_fburst17" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780636.sonic-head_DA-depleted-v3_CTX-favg14_fburst19" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780637.sonic-head_DA-depleted-v3_CTX-favg14_fburst21" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780638.sonic-head_DA-depleted-v3_CTX-favg14_fburst23" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780639.sonic-head_DA-depleted-v3_CTX-favg14_fburst25" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780640.sonic-head_DA-depleted-v3_CTX-favg14_fburst27" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780641.sonic-head_DA-depleted-v3_CTX-favg14_fburst29" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780642.sonic-head_DA-depleted-v3_CTX-favg14_fburst31" \
-    "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q1_const-rate_vary-freq/2018.08.02_job-780643.sonic-head_DA-depleted-v3_CTX-favg14_fburst50" \
-)
+outputs_clipboard="/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q5_sweep-gpe-gpe-ipsp/2018.08.02_job-780651.sonic-head_DA-depleted-v3_CTX-f0_GpeGpe-inh-x0.2
+/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q5_sweep-gpe-gpe-ipsp/2018.08.02_job-780652.sonic-head_DA-depleted-v3_CTX-f0_GpeGpe-inh-x0.4
+/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q5_sweep-gpe-gpe-ipsp/2018.08.02_job-780653.sonic-head_DA-depleted-v3_CTX-f0_GpeGpe-inh-x0.6
+/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q5_sweep-gpe-gpe-ipsp/2018.08.02_job-780654.sonic-head_DA-depleted-v3_CTX-f0_GpeGpe-inh-x0.8
+/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q5_sweep-gpe-gpe-ipsp/2018.08.02_job-780655.sonic-head_DA-depleted-v3_CTX-f0_GpeGpe-inh-x1.2
+/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q5_sweep-gpe-gpe-ipsp/2018.08.02_job-780656.sonic-head_DA-depleted-v3_CTX-f0_GpeGpe-inh-x1.4"
+readarray -t output_dirs <<< "${outputs_clipboard}"
+# output_dirs=(${outputs_clipboard//$'n'// }) # substitute newlines and make array
+# output_dirs=( \
+#     "/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q7_sweep-transmission-delay/2018.08.03_job-780788.sonic-head_DA-depleted-v3_CTX-f0_StnGpe-d4.0_GpeStn-d6.0" \
+# )
 
 # Generate output dirs from parent directory
 # parent_dir="/run/media/luye/Windows7_OS/Users/lkoelman/simdata-win/syn-v2_q3_const-freq_vary-burst-frac"
@@ -38,8 +33,8 @@ hpfreqs=($(for ((i=0;i<num_outputs;i++)); do echo 5; done))
 lpfreqs=($(for ((i=0;i<num_outputs;i++)); do echo 20; done))
 
 # Parameter sweep
-sweep_name="f_burst_ctx"
-sweep=(5 7 9 11 13 15 17 19 21 23 25 27 29 31 50)
+sweep_name="gsyn_gpe_gpe"
+sweep=(0.2 0.4 0.6 0.8 1.2 1.4)
 
 cd $nb_dir
 
@@ -48,13 +43,13 @@ for ((i=0;i<num_outputs;i++)); do
     outdir=${output_dirs[i]}
     hpfreq=${hpfreqs[i]}
     lpfreq=${lpfreqs[i]}
+    outfile="${outdir}/${nb_filename}"
 
     # Don't run notebook if exists
-    outfile="${outdir}/${nb_filename}"
-    if [ -e ${outfile} ]; then
-        echo -e "Skipping ${outdir}"
-        continue
-    fi
+    # if [ -e ${outfile} ]; then
+    #     echo -e "Skipping ${outdir}"
+    #     continue
+    # fi
 
     # notebook will read variables from Python configuration script
     echo $outdir >> $logfile
@@ -71,7 +66,7 @@ sweep_var_value = ${sweep[i]}"
     eval $nb_exec
 
     echo -e "Finished! Saving notebook to output directory ..."
-    nb_save="jupyter nbconvert ${outfile} --template=toc2 --output-dir=$outdir"
+    nb_save="jupyter nbconvert ${outfile} --template=toc2 --output-dir=${outdir}"
     eval $nb_save
 done
 
